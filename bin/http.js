826a1ef71085849255429b92407384a3d017fd8b
nafies1 <ghuroba.muslim@gmail.com>
2020-02-20 23:15:31 +0700

    handle until enter game

--- bin/http.js
+++ bin/http.js
@@ -1,6 +1,8 @@
 const http = require('../app')
 const io = require('socket.io')(http)
 const { Room }= require('../models')
+const axios = require('axios')
+let url
 
 io.on('connection', (socket) => {
   console.log('connect')
@@ -32,6 +34,29 @@ io.on('connection', (socket) => {
   socket.on('syncPlayers', (roomData) => {
     io.to(roomData.id).emit('syncRoomData', roomData)
   })
+
+  socket.on('startGame', (roomData) => {
+    io.to(roomData.id).emit('toGamePage')
+
+  })
+
+  socket.on('selectLevel', (payload) => {
+    console.log('select level:', payload)
+    url = `https://opentdb.com/api.php?amount=10&category=31&difficulty=${payload.level}&type=multiple`
+    axios({
+      method: 'get',
+      url
+    })
+      .then(({ data }) => {
+        console.log(data)
+        io.to(payload.id).emit('showQuestions', data)
+      })
+      .catch(({ response }) => {
+        console.log(response)
+      })
+  })
+  
+
 })
 http.listen(3000, () => {
   console.log('connect to 3000')
